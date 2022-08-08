80c0639943407a8266942a12ed6d836f2621af42
Andy Zheng <andyzheng679@gmail.com>
2022-08-07 20:43:13 -0400

    created left & right paddle, & the ball

--- pong.py
+++ pong.py
@@ -1,10 +1,38 @@
 import turtle
 
+#setting up 
 wn = turtle.Screen()
 wn.title("Pong in Python")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+#creating the left paddle
+left_paddle = turtle.Turtle()
+left_paddle.speed(0)
+left_paddle.shape("square")
+left_paddle.color("white")
+left_paddle.shapesize(stretch_wid=5, stretch_len=1)
+left_paddle.penup()
+left_paddle.goto(-350, 0)
+
+#creating the left paddle
+right_paddle = turtle.Turtle()
+right_paddle.speed(0)
+right_paddle.shape("square")
+right_paddle.color("white")
+right_paddle.shapesize(stretch_wid=5, stretch_len=1)
+right_paddle.penup()
+right_paddle.goto(350, 0)
+
+#creating the ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 
 while True:
     wn.update()
